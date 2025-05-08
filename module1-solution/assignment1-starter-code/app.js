a258e6e7952df0a09718ff1e6fa7d890b269248f
abdelac <me@gmail.com>
2025-05-08 18:44:46 +0100

    first 1

---
+++ module1-solution/assignment1-starter-code/app.js
@@ -0,0 +1,14 @@
+(function(){
+Angular.module("LunchCheck",[])
+.controller('LunchCheckController',LunchCheckController($scope)
+
+
+LunchCheckController.$inject=['$scope']
+function LunchCheckController($scope){
+	
+})
+
+
+
+
+})();
