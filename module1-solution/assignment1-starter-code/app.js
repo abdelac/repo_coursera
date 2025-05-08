276f93911255b14d0897434cf078dcd521b6ab09
abdelac <me@gmail.com>
2025-05-08 18:50:23 +0100

    first 1 2eme commit

--- module1-solution/assignment1-starter-code/app.js
+++ module1-solution/assignment1-starter-code/app.js
@@ -1,14 +1,42 @@
-(function(){
-Angular.module("LunchCheck",[])
-.controller('LunchCheckController',LunchCheckController($scope)
+(function () {
+  'use strict';
 
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject=['$scope']
-function LunchCheckController($scope){
-	
-})
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.lunchItems = '';
+    $scope.message = '';
+    $scope.messageClass = '';
+    $scope.borderClass = '';
 
+    $scope.checkLunch = function () {
+      if (!$scope.lunchItems || $scope.lunchItems.trim() === '') {
+        $scope.message = 'Please enter data first';
+        $scope.messageClass = 'message-red';
+        $scope.borderClass = 'input-red';
+        return;
+      }
 
+      const itemsArray = $scope.lunchItems
+        .split(',')
+        .map(item => item.trim())
+        .filter(item => item.length > 0); // removes empty items
 
-
+      if (itemsArray.length === 0) {
+        $scope.message = 'Please enter data first';
+        $scope.messageClass = 'message-red';
+        $scope.borderClass = 'input-red';
+      } else if (itemsArray.length <= 3) {
+        $scope.message = 'Enjoy!';
+        $scope.messageClass = 'message-green';
+        $scope.borderClass = 'input-green';
+      } else {
+        $scope.message = 'Too much!';
+        $scope.messageClass = 'message-green';
+        $scope.borderClass = 'input-green';
+      }
+    };
+  }
 })();
