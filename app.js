e57c8f9d72a0133457574cfe25d795656fd8cfc7
abdelac <me@gmail.com>
2025-05-08 18:18:35 +0100

    solution 1

---
+++ app.js
@@ -0,0 +1,21 @@
+(function (){
+  "use strict";
+
+angular.module('msg',[])
+.controller('msgcontroller',Ifunctin)
+
+msgcontroller.$inject=['$scope']
+function Ifunctin($scope){
+    $scope.name='message';
+    $scope.sc=u;
+
+  }
+
+$scope.sw=function(){
+  $scope.sc=t;
+};
+
+
+
+
+})();
